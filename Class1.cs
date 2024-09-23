7000df746e2e1c09aff3a099e8894d38911fad99
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-09-23 13:49:16 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,51 @@
+﻿using BepInEx;
+using SPT.Reflection.Patching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanVisor {
+    [BepInPlugin("Mattdokn.CleanVisor", "CleanVisor", "1.0.0")]
+    public class CleanVisor : BaseUnityPlugin {
+        private void Awake() {
+            new VisorEffectPatch().Enable();
+            new VisorEffectPatch2().Enable();
+        }
+    }
+
+    internal class VisorEffectPatch : ModulePatch {
+        protected override MethodBase GetTargetMethod() => typeof(VisorEffect).GetMethod("OnEnable");
+
+        [PatchPrefix]
+        public static void OnEnable(VisorEffect __instance) {
+            Console.WriteLine("IDK man cmon");
+            //__instance.Visible = false;
+        }
+    }
+
+    internal class VisorEffectPatch2 : ModulePatch {
+        protected override MethodBase GetTargetMethod() => typeof(VisorEffect).GetMethod("SetIntensity");
+
+        [PatchPrefix]
+        public static void SetIntensity(VisorEffect __instance, ref float intensity) {
+            Console.WriteLine($"RAAAA {intensity}");
+            intensity = 0.5f;
+            //__instance.Visible = false;
+        }
+    }
+
+    internal class VisorEffectPatch3 : ModulePatch {
+        
+        protected override MethodBase GetTargetMethod() => typeof(VisorEffect).GetMethod("method_2");
+
+        [PatchPrefix]
+        public static void method_2(VisorEffect __instance) {
+            Console.WriteLine($"gyuhg");
+            __instance.
+            //__instance.Visible = false;
+        }
+    }
+}
