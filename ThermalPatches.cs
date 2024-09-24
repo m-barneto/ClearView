0d4c255cb27de56b37c443c6f0c7e427241b931d
mBarneto <4347791+m-barneto@users.noreply.github.com>
2024-09-23 22:01:56 -0400

    Begin work on thermal

--- ThermalPatches.cs
+++ ThermalPatches.cs
@@ -1,6 +1,19 @@
 ﻿
+using SPT.Reflection.Patching;
+using System;
+using System.Reflection;
+
 namespace ClearView {
     internal class ThermalPatches {
+        //method_1
+        internal class ThermalOnEnable : ModulePatch {
+            protected override MethodBase GetTargetMethod() => typeof(ThermalVision).GetMethod("method_1");
 
+            [PatchPrefix]
+            public static void OnEnable(ThermalVision __instance) {
+                Console.WriteLine("Thermal enabled/disabled?");
+                Console.WriteLine(__instance.On);
+            }
+        }
     }
 }
--- VisorPatches.cs
+++ VisorPatches.cs
@@ -1,10 +1,5 @@
 ﻿using SPT.Reflection.Patching;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ClearView {
     internal class VisorPatches {
